a91a1faf6401ebbd8f18cf013c04216ab7d29432
nicksolony <nicksolony@gmail.com>
2023-11-11 22:47:02 -0500

    TS3 homework ready for submission

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,55 @@
+
+class Key {
+    private signature: number;
+    
+    constructor() {
+        this.signature = Math.random();
+    };
+
+    getSignature(): number {
+        return this.signature;
+    };
+};
+
+class Person {
+    constructor(private key:Key) {
+        this.key = key;
+    };
+
+    getKey(): Key {
+        return this.key;
+    };
+};
+
+abstract class House {
+
+    protected door: boolean;
+    protected key: Key;
+    private tenants: Person[];
+
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+        this.tenants = [];
+    };
+    
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        };
+    };
+    
+    public abstract openDoor(key: Key): void;
+};
+
+class MyHouse extends House{
+
+    public openDoor(key:Key) {
+        this.door = (key===this.key)
+    }
+};
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
